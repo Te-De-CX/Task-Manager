d1c307f2cbf6b6fa7fdc5f847ad82c1cc1af043e
teslim <tunjiheritage@gmail.com>
2025-02-16 04:03:11 +0100

    done

--- task-manager/src/Dashboard/Hero.tsx
+++ task-manager/src/Dashboard/Hero.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import { VscBell } from "react-icons/vsc";
 import { FaPlus } from "react-icons/fa6";
 
@@ -18,6 +18,37 @@ export default function Hero() {
           </div>
         </div>
         <div>
+          <div>
+            <h3>11</h3>
+            <p>hours worked</p>
+          </div>
+          <div>
+            <h3>$2.4k</h3>
+            <p>money paid</p>
+          </div>
+          <div>
+            <h3>36</h3>
+            <p>task done</p>
+          </div>
+        </div>
+        <div>
+          <h4>your statistics</h4>
+          <h5>worked hours</h5>
+          <p>my courses</p>
+          <div>
+            <p>weekly</p>
+            <div>
+              <input type="radio" name="" id="" />
+              <p>this week </p>
+              <input type="radio" name="" id="" />
+              <p>last week </p>
+            </div>
+          </div>
+          <div>
+            line chart
+          </div>
+        </div>
+        <div>
           <div className='flex justify-between items-center'>
             <input type="text" />
             <VscBell />
@@ -32,6 +63,35 @@ export default function Hero() {
             </p>
             <div>
             <FaPlus />
+            </div>
+          </div>
+        </div>
+        <div>
+          todays date
+          <p>delete</p>
+          <div>
+            left right
+          </div>
+          <div>
+            calender
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>daily standup call</h3>
+            <p>discuss team tasks for the day</p>
+            <div>
+              9:00am
+              <input type="checkbox" name="" id="" />
+            </div>
+          </div>
+          <div>
+            <h3>brand identity meeting</h3>
+            <p>discuss the brand identity guidlines for the print media</p>
+            <div>
+              11:00am
+              <input type="checkbox" name="" id="" />
+              <input type="checkbox" name="" id="" />
             </div>
           </div>
         </div>
